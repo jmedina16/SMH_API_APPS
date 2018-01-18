b9b0dc7b7d076acf8c1da8782af129afad765636
root <jmedina616@gmail.com>
2018-01-18 00:00:12 +0000

    latest update

--- apps/push/index.php
+++ apps/push/index.php
@@ -10,6 +10,13 @@ class push {
     }
 
     public function push_notification() {
+        $bsfPush = array(13373, 10012);
+        if (in_array($this->post_data['partner_id'], $bsfPush)) {
+            $this->bsfPush();
+        }
+    }
+
+    public function bsfPush() {
         $ks = $this->impersonate($this->post_data['partner_id']);
         $flavors_response = $this->get_flavors($ks, $this->post_data['entry_id']);
         $flavor = array();
@@ -26,11 +33,13 @@ class push {
         $final_push_data['partner_data'] = $this->post_data['partner_data'];
         $final_push_data['status'] = $this->post_data['status'];
         $final_push_data['flavors'] = $flavor;
+
+        $json_str = "jsonStr='" . json_encode($final_push_data) . "'";
         
-        $json_str = json_encode($final_push_data);
+        $json_str_test = 'jsonStr=\'{"partner_id":"13373","entry_id":"0_qwu778pt","name":"SampleVideo_1280x720_1mb","tags":"","thumbnail_url":"https:\/\/mediaplatform.streamingmediahosting.com\/p\/10012\/sp\/1001200\/thumbnail\/entry_id\/0_2us0xt65\/version\/0\/acv\/122","partner_data":"","status":"2","flavors":[{"id":"0_vq7freli","width":1280,"height":720,"bitrate":1590,"isSource":true,"isWeb":true,"status":2,"size":1034,"fileExt":"mp4"},{"id":"0_joj5pyq6","width":640,"height":360,"bitrate":447,"isSource":false,"isWeb":true,"status":2,"size":292,"fileExt":"mp4"},{"id":"0_dusampos","width":848,"height":480,"bitrate":754,"isSource":false,"isWeb":true,"status":2,"size":492,"fileExt":"mp4"},{"id":"0_ogjbtgbr","width":1024,"height":576,"bitrate":1383,"isSource":false,"isWeb":true,"status":2,"size":897,"fileExt":"mp4"},{"id":"0_ltrk6itf","width":1280,"height":720,"bitrate":1415,"isSource":false,"isWeb":true,"status":2,"size":918,"fileExt":"mp4"},{"id":"0_aagdfxk9","width":0,"height":0,"bitrate":128,"isSource":false,"isWeb":true,"status":2,"size":84,"fileExt":"mp3"}]}\'';
 
         $notification_url = 'http://clients.streamingmediahosting.com/medina/demos/listener/sync.php';
-        $response = $this->curlPostJson($notification_url, $json_str);
+        $response = $this->curlPostJson($notification_url, $json_str_test);
     }
 
     public function curlPostJson($url, $data) {
@@ -40,11 +49,14 @@ class push {
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_USERPWD, "test@liferay.com:test");
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             'Content-Type: application/json',
             'Content-Length: ' . strlen($data)
         ));
         $response = curl_exec($ch);
+        syslog(LOG_NOTICE, "SMH DEBUG : curlPostJson: " . print_r($response, true));
         curl_close($ch);
 
         return json_decode($response, true);

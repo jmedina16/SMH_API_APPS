10ad093deb4bddaa5f38521023edbf98c1f3e80d
root <jmedina616@gmail.com>
2018-02-13 05:00:13 +0000

    latest update

--- apps/channel/v1.0/schedule.php
+++ apps/channel/v1.0/schedule.php
@@ -22,49 +22,52 @@
             <script type="text/javascript">
                 var sessInfo = {pid: '<?php echo $pid; ?>', playerId: '<?php echo $playerId ?>'};
             </script>
-            <div class="container-fluid" id="player-wrapper">
-                <div class="row">
-                    <!--                <div style="border: 2px solid #000;width: 800px;height: 450px;overflow: hidden;float: left;padding: 10px;">-->
-                    <!--                    <div style="max-width: 800px;width: 100%;display: inline-block;position: relative;float: left;">-->
-                    <div class="col-xs-12 col-sm-7 col-md-5" style="margin-top: 10px;">
-                        <div style="margin-top: 56.25%;"></div>
-                        <div id="smh_player" style="position:absolute;top:0;left:0;left: 0;right: 0;bottom:0;padding-left:5px;padding-right:5px;"></div>
-                    </div>
-                    <!--                </div>-->
-                    <!--                    <div style="border: 2px solid #000;height: 450px;margin-left: 800px;">-->
-                    <div class="col-xs-12 col-sm-5 col-md-7" id="entry-details-wrapper">
-                        <div id="entry-details">
-                            <div id="event-title"></div>   
-                            <div id="channel-time"></div>
-                            <div id="event-desc"></div>
-                        </div>                        
-                    </div>
-                    <div class="clear"></div>                
-                </div>
-            </div>
-            <div id="scheduler" class="dhx_cal_container" style='width:100%; height:100%;'>
-                <div id="dhx_sticky_navline_wrapper">
-                    <div class="dhx_cal_navline">
-                        <div class="scheduler_zoom_out" onclick="smhS.zoomOut();"><i class="fa fa-search-minus" aria-hidden="true"></i></div>
-                        <div class="scheduler_zoom_in" onclick="smhS.zoomIn();"><i class="fa fa-search-plus" aria-hidden="true"></i></div>
-                        <div class="dhx_cal_prev_button">&nbsp;</div>
-                        <div class="dhx_cal_next_button">&nbsp;</div>
-                        <div class="dhx_cal_date"></div>
-                        <div class="dhx_minical_icon" id="dhx_minical_icon" onclick="smhS.show_minical()" style="left: 190px;">&nbsp;</div>
-                    </div>                 
-                </div>
-                <div id="dhx_sticky_header_wrapper">
-                    <div class="dhx_cal_header"></div>   
-                </div>                
-                <div class="dhx_cal_data"></div>		
-            </div>
-            <div class="modal fade" id="smh-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                <div class="modal-dialog smh-dialog">
-                    <div class="modal-content">
-                        <div class="modal-body"></div>
+            <div class="test">
+                <div class="container-fluid" id="player-wrapper">
+                    <div class="row">
+                        <!--                <div style="border: 2px solid #000;width: 800px;height: 450px;overflow: hidden;float: left;padding: 10px;">-->
+                        <!--                    <div style="max-width: 800px;width: 100%;display: inline-block;position: relative;float: left;">-->
+                        <div class="col-xs-12 col-sm-7 col-md-5" style="margin-top: 10px;">
+                            <div style="margin-top: 56.25%;"></div>
+                            <div id="smh_player" style="position:absolute;top:0;left:0;left: 0;right: 0;bottom:0;padding-left:5px;padding-right:5px;"></div>
+                        </div>
+                        <!--                </div>-->
+                        <!--                    <div style="border: 2px solid #000;height: 450px;margin-left: 800px;">-->
+                        <div class="col-xs-12 col-sm-5 col-md-7" id="entry-details-wrapper">
+                            <div id="entry-details">
+                                <div id="event-title"></div>   
+                                <div id="channel-time"></div>
+                                <div id="event-desc"></div>
+                            </div>                        
+                        </div>
+                        <div class="clear"></div>                
                     </div>
                 </div>
+                <div id="scheduler" class="dhx_cal_container" style='width:100%; height:100%;'>
+                    <div id="dhx_sticky_navline_wrapper">
+                        <div class="dhx_cal_navline">
+                            <div class="scheduler_zoom_out" onclick="smhS.zoomOut();"><i class="fa fa-search-minus" aria-hidden="true"></i></div>
+                            <div class="scheduler_zoom_in" onclick="smhS.zoomIn();"><i class="fa fa-search-plus" aria-hidden="true"></i></div>
+                            <div class="dhx_cal_prev_button">&nbsp;</div>
+                            <div class="dhx_cal_next_button">&nbsp;</div>
+                            <div class="dhx_cal_date"></div>
+                            <div class="dhx_minical_icon" id="dhx_minical_icon" onclick="smhS.show_minical()" style="left: 190px;">&nbsp;</div>
+                        </div>                 
+                    </div>
+                    <div id="dhx_sticky_header_wrapper">
+                        <div class="dhx_cal_header"></div>   
+                    </div>                
+                    <div class="dhx_cal_data"></div>		
+                </div>
+                <div class="modal fade" id="smh-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                    <div class="modal-dialog smh-dialog">
+                        <div class="modal-content">
+                            <div class="modal-body"></div>
+                        </div>
+                    </div>
+                </div>              
             </div>
+
             <script src="/js/jQuery-2.1.4.min.js?v=1" type="text/javascript"></script>
             <script src="//code.jquery.com/ui/1.10.3/jquery-ui.js?v=1"></script>
             <script src="/js/bootstrap.min.js?v=1" type="text/javascript"></script>

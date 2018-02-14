f59c338ad49a5be4b20e22d12db5671ea1764a28
root <jmedina616@gmail.com>
2018-02-14 06:00:27 +0000

    latest update

--- apps/channel/v1.0/schedule.php
+++ apps/channel/v1.0/schedule.php
@@ -92,14 +92,13 @@ if (isset($pid)) {
             <script src="//code.jquery.com/ui/1.10.3/jquery-ui.js?v=1"></script>
             <script src="<?php echo $apps_url ?>/p/<?php echo $pid ?>/js/bootstrap.min.js?v=1" type="text/javascript"></script>
             <script src="<?php echo $apps_url ?>/p/<?php echo $pid ?>/js/jquery.mCustomScrollbar.min.js?v=1" type="text/javascript"></script>
-            <script src="<?php echo $apps_url ?>/p/<?php echo $pid ?>/js/jquery.slimscroll.min.js?v=1" type="text/javascript"></script>
             <script src="<?php echo $apps_url ?>/p/<?php echo $pid ?>/js/dhtmlxscheduler.js?v=1.5" type = "text/javascript"></script>
             <script src="<?php echo $apps_url ?>/p/<?php echo $pid ?>/js/dhtmlxscheduler_limit.js?v=1.5" type="text/javascript"></script>
             <script src="<?php echo $apps_url ?>/p/<?php echo $pid ?>/js/dhtmlxscheduler_timeline.js?v=1.5" type="text/javascript"></script>
             <script src="<?php echo $apps_url ?>/p/<?php echo $pid ?>/js/dhtmlxscheduler_minical.js?v=1.5" type="text/javascript"></script>
             <script src="<?php echo $apps_url ?>/p/<?php echo $pid ?>/js/dhtmlxscheduler_tooltip.js?v=1.5" type="text/javascript"></script>
             <script src="<?php echo $apps_url ?>/p/<?php echo $pid ?>/js/dhtmlxscheduler_recurring.js?v=1.5" type="text/javascript"></script>
-            <script src="<?php echo $apps_url ?>/p/<?php echo $pid ?>/js/jquery.dotdotdot.js?v=1.5" type="text/javascript"></script>
+            <script src="<?php echo $apps_url ?>/p/<?php echo $pid ?>/js/jquery.dotdotdot.min.js?v=1.5" type="text/javascript"></script>
             <script src="<?php echo $apps_url ?>/p/<?php echo $pid ?>/js/schedule.js?v=1.5" type="text/javascript"></script>
         </body>
     </html>

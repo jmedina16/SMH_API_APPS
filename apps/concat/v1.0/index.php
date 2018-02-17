28a405db6ccf2699eb60c26f08ff9ac8316fd0cf
root <jmedina616@gmail.com>
2018-02-17 03:00:13 +0000

    latest update

--- apps/concat/v1.0/index.php
+++ apps/concat/v1.0/index.php
@@ -1,5 +1,12 @@
 <?php
 require 'vendor/autoload.php';
 
+$ffmpeg = FFMpeg\FFMpeg::create();
+$video = $ffmpeg->open( '/opt/kaltura/web/content/entry/data/10012/0_23vhczrn_0_9q4f1jgt_2.mp4' );
+$format = new FFMpeg\Format\Video\X264();
+$format->setAudioCodec("libmp3lame");
 
+$video
+    ->concat(array('/opt/kaltura/web/content/entry/data/10012/0_23vhczrn_0_9q4f1jgt_2.mp4', '/opt/kaltura/web/content/entry/data/10012/0_7dfoo2xk_0_hrrufbgp_2.mp4'))
+    ->saveFromDifferentCodecs($format, '/opt/kaltura/web/content/entry/data/10012/test.mp4');
 ?>

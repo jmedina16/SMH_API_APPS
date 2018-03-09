a7c79ca3d8a7401e226b5892f0eb3c9a8f8f178e
root <jmedina616@gmail.com>
2018-03-09 05:00:47 +0000

    latest update

---
+++ apps/cron/ppv_mem_users_cron.php
@@ -0,0 +1,24 @@
+<?php
+
+//header('Access-Control-Allow-Origin: *');
+class ppv_mem_users_cron {
+    
+    //run cron api
+    public function run() {
+        $this->curl_request($action, $args);
+    }
+
+    public function curl_request() {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://api.streamingmediahosting.com/index.php/api_dev/mem_user/logout_inactive_users?format=json");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
+    }
+
+}
+
+$pmuc = new ppv_mem_users_cron();
+$pmuc->run();
+?>
